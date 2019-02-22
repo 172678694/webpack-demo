a257af0f000738f16434f46af5093521544e4157
claire <172678694@qq.com>
2019-02-23 01:08:34 +0800

    作品轮播

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-//添加offset类
+//添加offset类,制造从下往上浮现的效果
 let specialTags = document.querySelectorAll('[data-x]')
 for (let i = 0; i < specialTags.length; i++) {
     specialTags[i].classList.add('offset')
 }
 findClosest()
 
-//sticky navbar & 高亮相应元素
+//sticky navbar & 监听onscroll事件，高亮相应元素
+
+//当文档在y方向上滚动的像素大于0（即开始滚动），topnavbar sticky
 window.onscroll = function (xxxx) {
     if (window.scrollY > 0) {
         topNavBar.classList.add('sticky')
@@ -16,28 +18,29 @@ window.onscroll = function (xxxx) {
 
 }
 
+//获取最靠近页面顶部的元素，即出现在用户眼前的元素
 function findClosest() {
     let specialTags = document.querySelectorAll('[data-x]')
     let minIndex = 0
-    for (let i = 1; i < specialTags.length; i++) {
+    //offsetTop 元素距离文档顶部的像素
+    for (let i = 1; i < specialTags.length; i++) { 
         if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)) {
-            minIndex = i
+            minIndex = i //minIndex就是离窗口顶部最近的元素
         }
     }
-    //minIndex就是离窗口顶部最近的元素
-    specialTags[minIndex].classList.remove('offset')
+    specialTags[minIndex].classList.remove('offset') 
 
     let id = specialTags[minIndex].id
     let a = document.querySelector('a[href="#' + id + '"]')
     let li = a.parentNode
     let brothersAndMe = li.parentNode.children
     for (let i = 0; i < brothersAndMe.length; i++) {
-        brothersAndMe[i].classList.remove('highlight')
+        brothersAndMe[i].classList.remove('highlight') 
     }
     li.classList.add('highlight')
 }
 
-//鼠标进入导航栏每个标签的效果/高亮
+//当鼠标进入onmouseenter导航栏每个li标签时出现下划线
 let liTags = document.querySelectorAll('nav.menu>ul>li')
 for (let i = 0; i < liTags.length; i++) {
     liTags[i].onmouseenter = function (x) {
@@ -80,3 +83,4 @@ for (let i = 0; i < aTags.length; i++) {
             .start();
     }
 }
+
